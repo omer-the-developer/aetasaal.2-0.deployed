07603bf3ffd78b5f88ed3d6913dde6314dbc8d99
root <root@ip-172-31-40-250.ec2.internal>
2024-06-13 06:08:35 +0000

    deployment

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-// import config from './config/index';
-// import bootstrap from './bootstrap/index';
-// import { startServer } from './server';
-// import { Logger } from './utils/logger';
-// import { loadTemplates } from './template/index';
-
-// const log = new Logger('aetasaal-api').createLogger({ env: config.env });
-
-// start();
-
-// async function start() {
-//   try {
-//     await bootstrap(log);
-//     await startServer(log);
-//     await loadTemplates();
-//   } catch (err) {
-//     log.error(err.message, 'error while application setup');
-//   }
-// }
-
-
-
 import config from './config/index'; // Import the config variable
 import { startServer } from './server'; // Change this line
 import { loadTemplates } from './template/index';
@@ -32,19 +10,12 @@ start();
 
 async function start() {
   try {
-
     await bootstrap(log);
-<<<<<<< HEAD
-<<<<<<< HEAD
+    //@ts-ignore
     await startServer(log);
-=======
-=======
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
-//@ts-ignore
-    await startServer(log); 
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
     await loadTemplates();
   } catch (err) {
     log.error(err.message, 'error while application setup');
   }
 }
+
--- src/server.ts
+++ src/server.ts
@@ -1,106 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import * as Bunyan from 'bunyan';
-// import * as Koa from 'koa';
-// import * as koaBody from 'koa-body';
-// import * as helmet from 'koa-helmet';
-// import * as mount from 'koa-mount';
-// import * as serve from 'koa-static';
-// import * as cors from 'koa2-cors';
-// import * as dotenv from 'dotenv';
-// import * as bunyanLogger from 'koa-bunyan-logger'; // Import koa-bunyan-logger
-// import * as path from 'path'; // Import path module to handle file paths
-
-// import config from './config/index';
-// import pagination from './middleware/pagination';
-// import errorMiddleware from './middleware/error';
-// import response from './middleware/response';
-// import routes from './route/index';
-// import { Logger } from './utils/logger';
-// import { Context, Middleware } from 'koa'; // Import Context, Middleware from Koa
-
-// // Load environment variables from .env file
-// dotenv.config();
-
-// const whitelist = [
-//   'http://localhost:4200',
-//   'http://localhost:3000',
-//   'http://localhost',
-//   'http://localhost:8100',
-//   'http://54.146.103.85:3000',
-//   'http://54.146.103.85:3000/upload',
-//   'http://54.146.103.85:3000/api',
-//   'http://54.146.103.85',
-//   'https://workable.aetasaal.com'
-// ];
-
-// function checkOriginAgainstWhitelist(ctx: Koa.Context) {
-//   const requestOrigin = ctx.request.headers.origin || ctx.request.origin;
-//   if (!whitelist.includes(requestOrigin)) {
-//     return ctx.throw(`${requestOrigin} is not a valid origin`);
-//   }
-//   return requestOrigin;
-// }
-
-// export async function startServer(log: Bunyan) {
-//   const app = new Koa();
-
-//   // Apply koaBunyanLogger middleware before any other middleware
-//   app.use(bunyanLogger(log)); // Use koa-bunyan-logger middleware
-
-//   // Set cache control headers manually
-//   app.use(async (ctx: Context, next: any) => {
-//     ctx.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-//     await next();
-//   });
-
-//   app.use(cors({ origin: checkOriginAgainstWhitelist }));
-
-//   app.use(koaBody({ jsonLimit: '10mb', formLimit: '50mb', multipart: true, json: true }));
-
-//   app.use(pagination);
-
-// // Serve the Angular application's static files
-// const frontendDir = path.join(__dirname, '..', 'aetesaal-omer', 'aetasaal-frontend-omer', 'dist', 'aetasaal-web');
-
-//   //@ts-ignore
-//   app.use(serve(frontendDir));
-
-//   app.use(errorMiddleware());
-
-//   // Database configuration
-//   const dbConfig = {
-//     // your database configuration
-//   };
-
-//   // Registers routes
-//   app.use(routes());
-//   app.use(response());
-
-//   return new Promise<void>((resolve, reject) => {
-//     const p = process.env.PORT || config.server.port;
-//     app.listen(p, () => {
-//       log.info('server started on port %d with env=%s', p, config.env);
-//       resolve();
-//     });
-
-//     app.on('error', err => {
-//       reject(err);
-//     });
-//   });
-// }
-
-
-
-import * as Bunyan from 'bunyan';
-=======
 import * as https from 'https';
 import * as fs from 'fs';
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
-=======
-import * as https from 'https';
-import * as fs from 'fs';
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
 import * as Koa from 'koa';
 import * as koaBody from 'koa-body';
 import * as cors from 'koa2-cors';
@@ -119,26 +18,12 @@ const whitelist = [
   'http://localhost:4200',
   'http://localhost:3000',
   'http://localhost:8100',
-<<<<<<< HEAD
-<<<<<<< HEAD
-  'http://54.146.103.85:3000',
-  'http://54.146.103.85:3000/upload',
-  'http://54.146.103.85:3000/api',
-  'http://54.146.103.85',
-  'https://workable.aetasaal.com'
-=======
-=======
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
   'http://34.199.172.154:3000',
   'http://34.199.172.154',
   'http://localhost:3000/upload',
   'https://aetasaal.duckdns.org',
   'https://aetasaal.duckdns.org:3000',
   'http://localhost:3000/api'
-<<<<<<< HEAD
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
-=======
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
 ];
 
 function checkOriginAgainstWhitelist(ctx: Koa.Context) {
@@ -149,44 +34,7 @@ function checkOriginAgainstWhitelist(ctx: Koa.Context) {
   return requestOrigin;
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export async function startServer(log: Bunyan) {
-  const app = new Koa();
-
-  app.use(bunyanLogger(log));
-
-  app.use(async (ctx: Context, next: any) => {
-    ctx.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-    await next();
-  });
-
-  app.use(cors({ origin: checkOriginAgainstWhitelist }));
-
-  app.use(koaBody({ jsonLimit: '10mb', formLimit: '50mb', multipart: true, json: true }));
-
-  app.use(pagination);
-
-  const frontendDir = path.join(__dirname, '..', 'aetesaal-omer', 'aetasaal-frontend-omer', 'dist', 'aetasaal-web');
-  app.use(serve(frontendDir));
-
-  app.use(errorMiddleware());
-
-  app.use(routes());
-  app.use(response());
-
-  return new Promise<void>((resolve, reject) => {
-    const p = process.env.PORT || config.server.port;
-    app.listen(p, () => {
-      log.info('server started on port %d with env=%s', p, config.env);
-      resolve();
-    });
-
-    app.on('error', err => {
-      reject(err);
-    });
-  });
-=======
+//@ts-ignore
 export async function startServer(log: Logger) {
   const app = new Koa();
 
@@ -224,62 +72,20 @@ export async function startServer(log: Logger) {
     app.use(response());
 
     return new Promise<void>((resolve, reject) => {
-
       const p = process.env.PORT || config.server.port;
-//@ts-ignore
-      server.listen(p, () => { log.info('server started on port %d with env=%s', p, config.env); resolve(); });
-      server.on('error', err => { reject(err); });
-    });
-  } catch (err) {
-    throw new Error(`Error reading SSL certificate and private key files: ${err.message}`);
-  }
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
-=======
-export async function startServer(log: Logger) {
-  const app = new Koa();
-
-  try {
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/aetasaal.duckdns.org/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/aetasaal.duckdns.org/cert.pem', 'utf8');
-    const ca = fs.readFileSync('/etc/letsencrypt/live/aetasaal.duckdns.org/chain.pem', 'utf8');
-
-    const httpsOptions = { key: privateKey, cert: certificate, ca: ca };
-
-    const server = https.createServer(httpsOptions, app.callback());
-
-    //@ts-ignore
-    app.use(cors({
       //@ts-ignore
-      origin: (ctx: Koa.Context) => {
-        // Explicitly allow requests from the specified origin with HTTPS
-        return 'https://aetasaal.duckdns.org';
-      },
-      credentials: true,
-      allowHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'appversion', 'platform'],
-    }));
-
-    //@ts-ignore
-    app.use(bunyanLogger(log));
-    app.use(async (ctx: Koa.Context, next: any) => {
-      ctx.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-      await next();
-    });
-
-    app.use(koaBody({ jsonLimit: '10mb', formLimit: '50mb', multipart: true, json: true }));
-    app.use(pagination);
-    app.use(errorMiddleware());
-    app.use(routes());
-    app.use(response());
-
-    return new Promise<void>((resolve, reject) => {
-
-      const p = process.env.PORT || config.server.port;
+      server.listen(p, () => {
 //@ts-ignore
-      server.listen(p, () => { log.info('server started on port %d with env=%s', p, config.env); resolve(); });
-      server.on('error', err => { reject(err); });
+        log.info('server started on port %d with env=%s', p, config.env);
+        resolve();
+      });
+
+      server.on('error', err => {
+        reject(err);
+      });
     });
   } catch (err) {
     throw new Error(`Error reading SSL certificate and private key files: ${err.message}`);
   }
->>>>>>> f4c32a6 (Program running on IP : Next Prob - Invalid Execution Id)
 }
+
